c34d54060676b09580f8b6f692a0554b33cb28f0
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-06 10:22:59 +0430

    add: reading file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// importing file and io libraries
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let selected_word = select_word();
+}
+
+fn select_word() -> String {
+    /* Open file */
+    let mut file = File::open("words.txt").expect("couldn't open file!");
+
+    /* Load file contents */
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)
+        .expect("an error occurred while reading file!");
+
+
+
 }
