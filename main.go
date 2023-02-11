85b920fe3a57437a79dafa4d2e4adc713c68d737
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-11 10:58:45 +0330

    add: fiber test

--- main.go
+++ main.go
@@ -1,27 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
+	"github.com/gofiber/fiber/v2"
 )
 
 type Sample struct {
-	Id    int    `tag:"none"`
-	Value string `tag:"display"`
+	Id       int    `json:"id" tag:"none"`
+	Value    string `json:"value" tag:"display"`
+	Metadata string `json:"-"`
 }
 
 func main() {
-	u := Sample{
-		Id:    44,
-		Value: "Value",
-	}
+	//u := Sample{
+	//	Id:    44,
+	//	Value: "Value",
+	//}
+	//
+	//t := reflect.TypeOf(u)
+	//
+	//for i := 0; i < t.NumField(); i++ {
+	//	f := t.Field(i)
+	//	if value, ok := f.Tag.Lookup("tag"); ok {
+	//		fmt.Println(value)
+	//	}
+	//}
+	app := fiber.New()
 
-	t := reflect.TypeOf(u)
-
-	for i := 0; i < t.NumField(); i++ {
-		f := t.Field(i)
-		if value, ok := f.Tag.Lookup("tag"); ok {
-			fmt.Println(value)
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		s := Sample{
+			Id:       44,
+			Value:    "value",
+			Metadata: "meta404",
 		}
+
+		return ctx.JSON(s)
+	})
+
+	if err := app.Listen(":8080"); err != nil {
+		panic(err)
 	}
 }
